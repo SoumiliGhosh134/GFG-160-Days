ac6da3e20f8dd03a8b3ece31c9de72ffec8e249e
SoumiliGhosh134 <soumilighosh134@gmail.com>
2025-05-29 12:18:50 +0530

    Create Problem_4.cpp

---
+++ Problem_4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    
+    void reverse(vector<int>& arr, int start, int end) {
+        while (start < end) {
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+    }
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+        int n = arr.size();
+        if (n == 0) return;
+        
+        d = d % n;
+        
+        // Step 1: Reverse first d elements
+        reverse(arr, 0, d - 1);
+        // Step 2: Reverse the rest
+        reverse(arr, d, n - 1);
+        // Step 3: Reverse the whole array
+        reverse(arr, 0, n - 1);
+    }
+};
