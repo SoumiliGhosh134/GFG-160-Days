2314a6b25450db992156becd1dc7f9dc81b2798b
SoumiliGhosh134 <soumilighosh134@gmail.com>
2025-05-29 12:13:45 +0530

    Create Problem_2.cpp

---
+++ Problem_2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int nonZeroIndex = 0;
+        
+        // Move all non-zero elements to the front
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[nonZeroIndex++ ] = arr[i];
+                
+            }
+        }
+        
+        // Fill remaining positions with zeros
+        while (nonZeroIndex < n) {
+            arr[nonZeroIndex++] = 0;
+        }
+    }
+};
