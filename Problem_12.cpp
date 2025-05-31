06cbe8b75cfeaf60e93e8ea3cb5f2b861ce64e84
SoumiliGhosh <soumilighosh134@gmail.com>
2025-05-31 12:37:33 +0530

    Create Problem_12.cpp

---
+++ Problem_12.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    // arr: input array
+    // Function to find maximum circular subarray sum.
+    int circularSubarraySum(vector<int> &arr) {
+
+
+       int n = arr.size();
+       int totalSum = arr[0];
+       
+       
+       int maxKadane = arr[0], currMax = arr[0];
+       int minKadane = arr[0], currMin = arr[0];
+       
+       for (int i = 1; i < n; i++) {
+          currMax = max(arr[i], currMax + arr[i]);
+          maxKadane = max(maxKadane, currMax);
+          
+          currMin = min(arr[i], currMin + arr[i]);
+          minKadane = min(minKadane, currMin);
+          
+          totalSum += arr[i];
+          
+       }
+       
+       // Edge case: All elements are negative
+       if (maxKadane < 0) return maxKadane;
+            
+            
+        return max(maxKadane, totalSum - minKadane);    
+       
+    
+    }
+};
