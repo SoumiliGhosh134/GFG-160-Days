3cb5de7d4561c0c9079473dd79c9a6e726258c9b
SoumiliGhosh <soumilighosh134@gmail.com>
2025-06-02 23:15:25 +0530

    Create Problem_16.cpp

---
+++ Problem_16.cpp
@@ -0,0 +1,29 @@
+/*
+ * Name: Soumili Ghosh
+ * Github: SoumiliGhosh134
+ * Linkedln: https://www.linkedin.com/in/soumili-ghosh-504a75313/
+ */
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        if (s1.length() != s2.length()) return false;
+        
+        int count[26] = {0};
+        
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1[i] - 'a']++;
+            count[s2[i] - 'a']--;
+            
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) 
+                return false;
+                
+        }
+        
+        
+        return true;
+    }
+};
