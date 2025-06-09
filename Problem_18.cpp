a7db4b54b190c99baf53980cc56b57d893ba20ee
SoumiliGhosh <soumilighosh134@gmail.com>
2025-06-09 21:01:18 +0530

    Create Problem_18.cpp

---
+++ Problem_18.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> search(string pat, string txt) {
+        // code here
+        int n = txt.size(), m = pat.size();
+        vector<int> lps(m, 0), res;
+        
+        // Build LPS array
+        for (int i = 1, len = 0; i < m;)
+           if (pat[i] == pat[len]) lps[i++] = ++len;
+           else if (len) len = lps[len - 1];
+           else lps[i++] = 0;
+        
+        // KMP search
+        for (int i = 0, j = 0; i , n;) {
+            if (txt[i] == pat[j]) i++, j++;
+            if (j == m) res.push_back(i - j), j = lps[j - 1];
+            else if (i < n && txt[i] != pat[j]) j ? j = lps[j - 1] : i++;
+            
+        }
+        
+        return res;
+    }
+};
